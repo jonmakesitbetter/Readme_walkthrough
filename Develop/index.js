fe66a6aa1ce50921726d2baacea9a0387d8e54d1
unknown <jonmakesitbetter@gmail.com>
2020-09-23 21:23:16 -0400

    Initial commit, wrote questions array

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+// array of questions for user
+const questions = [
+    "What is your project title?",
+    "What is the description of your project?",
+    "What is in your table of contents?",
+    "What are  your installation instructions?",
+    "What are you usage instructions?",
+    "What license are you using?",
+    "What contributors will you list in your project?",
+    "What tests would you like to include?",
+    "What questions would you like to include?"
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
