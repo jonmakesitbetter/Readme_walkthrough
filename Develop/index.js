967a81343ec852e522797c7b4f3dbe44f3084077
unknown <jonmakesitbetter@gmail.com>
2020-09-23 22:30:22 -0400

    Filled in type/message/name objects in inquirer .prompt

--- Develop/index.js
+++ Develop/index.js
@@ -3,17 +3,58 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-    "What is your project title?",
-    "What is the description of your project?",
-    "What is in your table of contents?",
-    "What are  your installation instructions?",
-    "What are you usage instructions?",
-    "What license are you using?",
-    "What contributors will you list in your project?",
-    "What tests would you like to include?",
-    "What questions would you like to include?"
-];
+{
+    type: "input",
+    message: "What is your project title?",
+    name:"Title"
+}
+{
+    type: "input",
+    message: "What is the description of your project?",
+    name:"Description"
+}
+{
+    type: "input",
+    message:"What is in your table of contents?",
+    name:"Table of Contents"
+}
+{
+    type: "input",
+    message:"What are  your installation instructions?",
+    name:"Installation"
+}
+{
+    type: "input",
+    message:"What are you usage instructions?",
+    name:"Instructions"
+}
+{
+    type: "input",
+    message:"What license are you using?",
+    name:"License"
+}
+{
+    type: "input",
+    message:"What contributors will you list in your project?",
+    name:"Contributors"
+}
+{
+    type: "input",
+    message:"What tests would you like to include?",
+    name:"Tests"
+}
+{
+    type: "input",
+    message:"What questions would you like to include?",
+    name:"Questions"
+}
 
+];
+inquirer
+.prompt({
+    //we have an array of questions, so we need to ask them. we
+    //could ask them via a loop, 
+})
 // function to write README file
 function writeToFile(fileName, data) {
 }
